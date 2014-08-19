cfb702aed5cb917013c949e14b468aba0bc904d6
Daniel Hudy <djh3315@gmail.com>
2014-08-19 09:28:48 -0400

    Testing account verification

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -1,8 +1,10 @@
 require 'net/http'
 class AnimesController < ApplicationController
   def index
-    url = URI.parse('http://myanimelist.net/api/anime/search.xml?q=bleach')
+    url = URI.parse('http://myanimelist.net/api/account/verify_credentials.xml')
     req = Net::HTTP::Get.new(url)
+    req.add_field('user', 'djh3315')
+    req.add_field('password', 'apitest')
     req.add_field('user-agent', 'api-indiv-BDF02918341A555C9108D3AC352A2432')
     res = Net::HTTP.start(url.host, url.port) { |http|
       http.request(req)
