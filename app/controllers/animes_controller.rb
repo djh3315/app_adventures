459a50fce989889c9a76445ade8473e911cfba36
Daniel Hudy <djh3315@gmail.com>
2014-08-19 09:43:36 -0400

    please god work

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -6,11 +6,11 @@ class AnimesController < ApplicationController
     req.add_field('user', 'djh3315')
     req.add_field('password', 'apitest')
     req.add_field('user-agent', 'api-indiv-BDF02918341A555C9108D3AC352A2432')
-    @query = req.get_field('user-agent')
+    @query = req.inspect
     res = Net::HTTP.start(url.host, url.port) { |http|
       http.request(req)
     }
-    @response = res.inspect
+    @response = res.message
   end
   def create
     # User Agent - api-indiv-BDF02918341A555C9108D3AC352A2432
