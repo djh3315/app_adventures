bd664c765dd86c660a7dfa8586d785590e74632c
Daniel Hudy <djh3315@gmail.com>
2014-08-19 11:08:07 -0400

    Back to Net/HTTP

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -1,16 +1,18 @@
 require 'net/http'
-require 'rest_client'
 
 class AnimesController < ApplicationController
   def index
-    res = RestClient.get 'http://myanimelist.net/api/anime/search.xml?q=bleach', {:user-agent => 'api-indiv-BDF02918341A555C9108D3AC352A2432'}
+    # res = RestClient.get 'http://myanimelist.net/api/anime/search.xml?q=bleach', {:user-agent => 'api-indiv-BDF02918341A555C9108D3AC352A2432'}
     # res = RestClient.get 'http://myanimelist.net/api/account/verify_credentials.xml', {:user-agent => 'api-indiv-BDF02918341A555C9108D3AC352A2432', :user => 'djh3315', :password => 'apitest'}
     # url = URI.parse()
     # req = Net::HTTP::Get.new(url)
     # req.add_field('user', 'djh3315')
     # req.add_field('password', 'apitest')
     # req.add_field('user-agent', 'api-indiv-BDF02918341A555C9108D3AC352A2432')
-    @response = res.inspect
+    http = Net::HTTP.new("myanimelist.net")
+    req = Net::HTTP::Get.new("/api/anime/search.xml?q=bleach", {'user-agent' => 'api-indiv-BDF02918341A555C9108D3AC352A2432'})
+    res = http.request(req)
+    @response = res.body
   end
   def create
     # User Agent - api-indiv-BDF02918341A555C9108D3AC352A2432
