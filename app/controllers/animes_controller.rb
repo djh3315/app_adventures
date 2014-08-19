f3f859c58c20eefab5fe489f5ba5845189831263
Daniel Hudy <djh3315@gmail.com>
2014-08-19 12:37:16 -0400

    trying to fix server...

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -9,11 +9,11 @@ class AnimesController < ApplicationController
     # req.add_field('user', 'djh3315')
     # req.add_field('password', 'apitest')
     # req.add_field('user-agent', 'api-indiv-BDF02918341A555C9108D3AC352A2432')
-    http = Net::HTTP.new("myanimelist.net")
-    req = Net::HTTP::Get.new("/api/anime/search.xml?q=bleach", {'user-agent' => 'api-indiv-BDF02918341A555C9108D3AC352A2432', 'user' => 'djh3315', 'password' => 'apitest'})
-    res = http.request(req)
-    @code = res.code
-    @response = res.body
+    # http = Net::HTTP.new("myanimelist.net")
+    # req = Net::HTTP::Get.new("/api/anime/search.xml?q=bleach", {'user-agent' => 'api-indiv-BDF02918341A555C9108D3AC352A2432', 'user' => 'djh3315', 'password' => 'apitest'})
+    # res = http.request(req)
+    # @code = res.code
+    # @response = res.body
   end
   def create
     # User Agent - api-indiv-BDF02918341A555C9108D3AC352A2432
