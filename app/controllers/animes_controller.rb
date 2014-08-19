10c48f6efa1f21af0b478be52202136e0a658874
Daniel Hudy <djh3315@gmail.com>
2014-08-19 08:58:53 -0400

    rendering xml not json, whoops

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -17,6 +17,6 @@ class AnimesController < ApplicationController
     res = Net::HTTP.start(url.host, url.port) { |http|
       http.request(req)
     }
-    render :json => req.body
+    render :xml => req.body
   end
 end
