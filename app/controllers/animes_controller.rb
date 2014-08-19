f26ccd97f8ef747798479296401b3f8d1fff1cbe
Daniel Hudy <djh3315@gmail.com>
2014-08-19 09:05:39 -0400

    moar changes

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -7,7 +7,6 @@ class AnimesController < ApplicationController
     res = Net::HTTP.start(url.host, url.port) { |http|
       http.request(req)
     }
-    render :json => req.body
   end
   def create
     # User Agent - api-indiv-BDF02918341A555C9108D3AC352A2432
@@ -17,6 +16,5 @@ class AnimesController < ApplicationController
     res = Net::HTTP.start(url.host, url.port) { |http|
       http.request(req)
     }
-    render req.success?
   end
 end
