cb80e5c8b4a5e66fc7890162b001ba1746a4a4b6
Daniel Hudy <djh3315@gmail.com>
2014-08-19 11:14:19 -0400

    Adding in error code

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -12,6 +12,7 @@ class AnimesController < ApplicationController
     http = Net::HTTP.new("myanimelist.net")
     req = Net::HTTP::Get.new("/api/anime/search.xml?q=bleach", {'user-agent' => 'api-indiv-BDF02918341A555C9108D3AC352A2432', 'user' => 'djh3315', 'password' => 'apitest'})
     res = http.request(req)
+    @code = res.code
     @response = res.body
   end
   def create
