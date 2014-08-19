3f85fa1a432117d6d7e83a53f7d91437afbb159b
Daniel Hudy <djh3315@gmail.com>
2014-08-19 09:53:42 -0400

    Now using rest-client gem

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -1,16 +1,14 @@
 require 'net/http'
+require 'rest_client'
 class AnimesController < ApplicationController
   def index
-    url = URI.parse('http://myanimelist.net/api/account/verify_credentials.xml')
-    req = Net::HTTP::Get.new(url)
-    req.add_field('user', 'djh3315')
-    req.add_field('password', 'apitest')
-    req.add_field('user-agent', 'api-indiv-BDF02918341A555C9108D3AC352A2432')
-    @query = req.inspect
-    res = Net::HTTP.start(url.host, url.port) { |http|
-      http.request(req)
-    }
-    @response = res.message
+    res = RestClient.get 'http://myanimelist.net/api/account/verify_credentials.xml', {:user-agent => 'api-indiv-BDF02918341A555C9108D3AC352A2432', :user => 'djh3315', :password => 'apitest'}
+    # url = URI.parse()
+    # req = Net::HTTP::Get.new(url)
+    # req.add_field('user', 'djh3315')
+    # req.add_field('password', 'apitest')
+    # req.add_field('user-agent', 'api-indiv-BDF02918341A555C9108D3AC352A2432')
+    @response = res.inspect
   end
   def create
     # User Agent - api-indiv-BDF02918341A555C9108D3AC352A2432
