4f9c12c1ca936e26d7b00b454fc1e7d632f28fae
Daniel Hudy <djh3315@gmail.com>
2014-08-19 11:12:00 -0400

    Attempting to fix invalid credentials

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -10,7 +10,7 @@ class AnimesController < ApplicationController
     # req.add_field('password', 'apitest')
     # req.add_field('user-agent', 'api-indiv-BDF02918341A555C9108D3AC352A2432')
     http = Net::HTTP.new("myanimelist.net")
-    req = Net::HTTP::Get.new("/api/anime/search.xml?q=bleach", {'user-agent' => 'api-indiv-BDF02918341A555C9108D3AC352A2432'})
+    req = Net::HTTP::Get.new("/api/anime/search.xml?q=bleach", {'user-agent' => 'api-indiv-BDF02918341A555C9108D3AC352A2432', 'user' => 'djh3315', 'password' => 'apitest'})
     res = http.request(req)
     @response = res.body
   end
