7d7e4020b9429a3d9338e3e4b244c63a776822a0
Daniel Hudy <djh3315@gmail.com>
2014-08-19 08:51:01 -0400

    Attempting to hit MAL api

---
+++ app/controllers/animes_controller.rb
@@ -0,0 +1,13 @@
+require 'net/http'
+class AnimesController < ApplicationController
+  def create
+    # User Agent - api-indiv-BDF02918341A555C9108D3AC352A2432
+    url = URI.parse('http://myanimelist.net/api/anime/search.xml?q=bleach')
+    req = Net::HTTP::Get.new(url)
+    req.add_field('user-agent', 'api-indiv-BDF02918341A555C9108D3AC352A2432')
+    res = Net::HTTP.start(url.host, url.port) { |http|
+      http.request(req)
+    }
+    render :json => req.body
+  end
+end
