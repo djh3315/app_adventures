3c09af3211d40f3057c95165f484dbf3685ea5f1
Daniel Hudy <djh3315@gmail.com>
2014-08-19 08:52:12 -0400

    changed the test to a GET route

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -1,5 +1,14 @@
 require 'net/http'
 class AnimesController < ApplicationController
+  def index
+    url = URI.parse('http://myanimelist.net/api/anime/search.xml?q=bleach')
+    req = Net::HTTP::Get.new(url)
+    req.add_field('user-agent', 'api-indiv-BDF02918341A555C9108D3AC352A2432')
+    res = Net::HTTP.start(url.host, url.port) { |http|
+      http.request(req)
+    }
+    render :json => req.body
+  end
   def create
     # User Agent - api-indiv-BDF02918341A555C9108D3AC352A2432
     url = URI.parse('http://myanimelist.net/api/anime/search.xml?q=bleach')
